70a57d12aab42833cdbb3efcb826341ab2a816e2
Zaide Kurti <zaiido1920@gmail.com>
2023-02-08 17:09:32 +0100

    Code refactoring

--- src/App.js
+++ src/App.js
@@ -4,10 +4,9 @@ import './App.css';
 import Footer from './components/Footer';
 import MovieDetails from './components/MovieDetails';
 import NetflixNavbar from './components/NetflixNavbar';
-import NewReleases from './components/NewReleases';
+import PageNotFound from './components/PageNotFound';
 import TrendingNowSection from './components/TrendingNowSection';
 import TvShows from './components/TvShows';
-import WatchItAgain from './components/WatchItAgain';
 
 function App() {
   return (
@@ -17,16 +16,9 @@ function App() {
         <div className='overflow-hidden'>
           <Routes>
             <Route element={<TrendingNowSection />} path="/" />
-          </Routes>
-          <Routes>
-            <Route element={<WatchItAgain />} path="/" />
-          </Routes>
-          <Routes>
-            <Route element={<NewReleases />} path="/" />
-          </Routes>
-          <Routes>
             <Route element={<TvShows />} path='/tv-shows' />
             <Route element={<MovieDetails />} path='/movie-details/:movieId' />
+            <Route element={<PageNotFound />} path="*" />
           </Routes>
           <Footer />
         </div>
--- src/components/TrendingNowSection.jsx
+++ src/components/TrendingNowSection.jsx
@@ -1,35 +1,41 @@
 import { Component } from "react";
 import { Carousel, Container, Row } from "react-bootstrap";
 import BuildingGallery from "./BuildingGallery";
+import NewReleases from "./NewReleases";
+import WatchItAgain from "./WatchItAgain";
 class TrendingNowSection extends Component {
     render() {
         return (
-            <section id="trending-now">
-                <Container fluid>
-                    <div className="my-3 align-container">
-                        <div className="d-flex align-items-center mb-3 explore">
-                            <h3 className="font-weight-bolder mr-2">Trending Now</h3>
+            <>
+                <section id="trending-now">
+                    <Container fluid>
+                        <div className="my-3 align-container">
+                            <div className="d-flex align-items-center mb-3 explore">
+                                <h3 className="font-weight-bolder mr-2">Trending Now</h3>
+                            </div>
+                            <Carousel interval={null} indicators={false}>
+                                <Carousel.Item>
+                                    <Row className="movies-container">
+                                        <BuildingGallery query="james%20bond" />
+                                    </Row>
+                                </Carousel.Item>
+                                <Carousel.Item>
+                                    <Row className="movies-container">
+                                        <BuildingGallery query="twilight" />
+                                    </Row>
+                                </Carousel.Item>
+                                <Carousel.Item>
+                                    <Row className="movies-container">
+                                        <BuildingGallery query="harry%20potter" />
+                                    </Row>
+                                </Carousel.Item>
+                            </Carousel>
                         </div>
-                        <Carousel interval={null} indicators={false}>
-                            <Carousel.Item>
-                                <Row className="movies-container">
-                                    <BuildingGallery query="james%20bond" />
-                                </Row>
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <Row className="movies-container">
-                                    <BuildingGallery query="twilight" />
-                                </Row>
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <Row className="movies-container">
-                                    <BuildingGallery query="harry%20potter" />
-                                </Row>
-                            </Carousel.Item>
-                        </Carousel>
-                    </div>
-                </Container>
-            </section>
+                    </Container>
+                </section>
+                <WatchItAgain />
+                <NewReleases />
+            </>
         )
     }
 }
